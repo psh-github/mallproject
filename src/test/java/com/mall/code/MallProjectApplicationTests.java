1e859b3034264ec048cbcc055e7c2385bf86aa7d
psh-github <sanghyun1206@naver.com>
2022-05-18 14:02:09 +0900

    프로젝트 생성

---
+++ src/test/java/com/mall/code/MallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
